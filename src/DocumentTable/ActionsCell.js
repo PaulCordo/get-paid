9298fae0f419a9a9e24729d88177ca88222bcbff
paul <paul@paulcordonnier.com>
2022-03-22 23:57:54 +0100

    bigger action cell min size

--- src/DocumentTable/ActionsCell.js
+++ src/DocumentTable/ActionsCell.js
@@ -2,9 +2,10 @@ import React from "react";
 
 import { DocumentActionButtons } from "../DocumentActionButtons";
 
+const actionCount = 4;
 export function ActionsCell({ value: document }) {
   return (
-    <div style={{ minWidth: 42 * 3 + 16 * 2 + 1 }}>
+    <div style={{ minWidth: 42 * actionCount + 16 * (actionCount - 1) + 1 }}>
       <DocumentActionButtons document={document} />
     </div>
   );
