416d0eda811f835965fa7e0cac34a0ca1950df69
Paul Cordonnier <paul@paulcordonnier.com>
2022-05-31 11:06:29 +0200

    5 action count in ActionCell

--- src/DocumentTable/ActionsCell.js
+++ src/DocumentTable/ActionsCell.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { DocumentActionButtons } from "../DocumentActions";
 
-const actionCount = 4;
+const actionCount = 5;
 export function ActionsCell({ value: document }) {
   return (
     <div style={{ minWidth: 42 * actionCount + 16 * (actionCount - 1) + 1 }}>
