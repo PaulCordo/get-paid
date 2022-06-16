88be42ed443216a6666c767a6bb56bd86dca4eaa
Paul Cordonnier <paul@paulcordonnier.com>
2022-06-16 16:33:18 +0200

    Quit app on all windows closed (even for MacOS)

--- public/electron.js
+++ public/electron.js
@@ -37,9 +37,7 @@ function createWindow() {
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("activate", () => {
